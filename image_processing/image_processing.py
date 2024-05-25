3e6dea21aa3cc40ba7a9a79bbc5aee12e92df48a
H8sGR8 <mateuszkobalczyk8@gmailcom>
2024-05-25 22:06:35 +0200

    task1

--- image_processing/image_processing.py
+++ image_processing/image_processing.py
@@ -1,4 +1,17 @@
 from PIL import Image
+import numpy as np
 
-with Image.open("yoda.jpeg") as image:
-    pass
+
+class ReadingImage:
+
+    def __init__(self, image):
+        self.image = Image.open(image)
+        self.pixels = np.array(self.image)
+
+    def show_image(self):
+        Image.fromarray(self.pixels).show()
+
+
+if __name__ == "__main__":
+    yoda = ReadingImage("yoda.jpeg")
+    yoda.show_image()
